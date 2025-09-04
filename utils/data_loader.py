78d936de843555f0a18ae3bb4b58fab064880bff
Camden Rush <rush0camden@gmail.com>
2025-09-03 21:32:43 -0500

    google sheet sync

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -17,8 +17,8 @@ def load_students():
         with open("students.csv", newline='', encoding='utf-8') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                first_name = row['first name'].strip()
-                last_name = row['last name'].strip()
+                first_name = row['first_name'].strip()
+                last_name = row['last_name'].strip()
                 lower_full_name = f"{first_name.lower()} {last_name.lower()}"
                 original_full_name = f"{first_name} {last_name}"
                 students[lower_full_name] = {
@@ -30,5 +30,5 @@ def load_students():
         print("error: students.csv not found.")
         return {}
     except KeyError:
-        print("error: CSV must have columns: 'first name', 'last name', 'teams'.")
+        print("error: CSV must have columns: 'first_name', 'last_name', 'teams'.")
         return {}
